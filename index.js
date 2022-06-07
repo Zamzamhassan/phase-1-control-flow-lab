72b9aa6780c78130ba0a5635e0b5e74353e9c3a2
Zamzam <Zamzamhassan44@gmail.com>
2022-06-07 08:51:29 +0300

    if else

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance<=400)
+  {
+   return 'This one is on me!';
+  }
+  else if(distance <= 2499)
+  {
+  return 'I will gladly take your thirty bucks.';
+  }
+  else
+  {
+ return 'No can do.';
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
 }
-
-function switchOnCharmFromTip(){
   // Write your code here!
+  function ternaryCheckCity(city){
+    return city === 'NYC'? "Ok, sounds good.": "No go.";
+  }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything' :
+      return 'Bye.';
+  }
 }
